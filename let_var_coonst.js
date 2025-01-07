9afc2707345394d0a37f26eb89daba8101e802a4
Sujal Warke <2024.sujalw@isu.ac.in>
2025-01-07 23:57:23 +0530

    Add files via upload

---
+++ let_var_coonst.js
@@ -0,0 +1,29 @@
+
+
+const name;  //declaration
+name = "Nick" //initialization
+console.log(name)
+
+let name;
+name = "Max"
+console.log(name)
+
+
+
+// var name = "something"
+//
+//
+//
+// const birthYear = 1990;
+//
+// const birthMonth = 1983;
+
+
+
+
+
+
+
+
+
+
