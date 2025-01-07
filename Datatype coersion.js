7f5aa6b9c0673dfb744147b5b5fb0b48999f387a
Sujal Warke <2024.sujalw@isu.ac.in>
2025-01-07 23:59:16 +0530

    Update Datatype coersion.js

--- Datatype coersion.js
+++ Datatype coersion.js
@@ -1,24 +1,24 @@
-// Implicit Coercion
+Implicit Coercion
 
-// let result = "5" + 2
-// console.log(result)
-// console.log(typeof result)
-//
-// let diff = "5" - 2;
-// console.log(diff)
-// console.log(typeof diff)
+let result = "5" + 2
+console.log(result)
+console.log(typeof result)
 
-
-//explicit coercion
-// let result = Number("5")
-// console.log(result)
-// console.log(typeof result)
+let diff = "5" - 2;
+console.log(diff)
+console.log(typeof diff)
 
 
+explicit coercion
+let result = Number("5")
+console.log(result)
+console.log(typeof result)
 
-// let str = String(5)
-// console.log(str)
-// console.log(typeof str)
+
+
+let str = String(5)
+console.log(str)
+console.log(typeof str)
 
 
 let bool = Boolean(0)  // 1 - True & 0 - False
