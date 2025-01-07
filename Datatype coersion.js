2340026709b7ae9da0ab9ecd55179abca67b1214
Sujal Warke <2024.sujalw@isu.ac.in>
2025-01-07 23:57:57 +0530

    Add files via upload

---
+++ Datatype coersion.js
@@ -0,0 +1,27 @@
+// Implicit Coercion
+
+// let result = "5" + 2
+// console.log(result)
+// console.log(typeof result)
+//
+// let diff = "5" - 2;
+// console.log(diff)
+// console.log(typeof diff)
+
+
+//explicit coercion
+// let result = Number("5")
+// console.log(result)
+// console.log(typeof result)
+
+
+
+// let str = String(5)
+// console.log(str)
+// console.log(typeof str)
+
+
+let bool = Boolean(0)  // 1 - True & 0 - False
+console.log(bool)
+console.log(typeof bool)
+
