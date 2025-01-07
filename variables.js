46d02a8aed06e9ff4fab1fa09a4d6b51bafcae3b
Sujal Warke <2024.sujalw@isu.ac.in>
2025-01-07 23:57:47 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,18 @@
+//Variables
+
+let num = 25;  // let/var/const variable_name = value;
+// console.log(num);
+// console.log(typeof num);
+//
+// console.log("")
+//
+// let name = "Max"
+// console.log(name)
+// console.log(typeof name);
+
+
+var name;  //declaration
+name = "Nick" //initialization
+console.log(name)
+
+
