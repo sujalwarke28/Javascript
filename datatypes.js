2c4b242b2d7c68588476687cca1326c3fdc23d29
Sujal Warke <2024.sujalw@isu.ac.in>
2025-01-07 23:57:31 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,37 @@
+let score = 95.5; //number               N
+console.log(score)
+console.log(typeof score)
+
+let message = "This is a string"
+console.log(message)
+console.log(typeof message)
+
+let isValid = true;
+console.log(isValid)
+console.log(typeof isValid)
+
+let uninitializedVar;
+console.log(uninitializedVar)
+console.log(typeof uninitializedVar)
+
+let emptyValue = null;
+console.log(null)
+console.log(typeof null)
+
+
+
+//Complex datatypes
+
+//Object Data Type
+let person={
+    name : "Alice",
+    age: 30
+
+}
+console.log(person)
+
+//Array
+
+let colors = ["red", "green", "blue"];
+console.log(colors)
+
