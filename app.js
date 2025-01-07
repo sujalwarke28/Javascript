1af9d42fee153f8531de7d5a12fbd40fc9ee98ca
Sujal Warke <2024.sujalw@isu.ac.in>
2025-01-07 23:56:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+function Greeter() {
+  return "Hello People";
+}
+export default function App() {
+  return (
+    <div className="App">
+      <Greeter />
+    </div>
+  );
+}
